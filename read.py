9b45a50c06a6d7f39706c241a5e40d7f3827032c
nick <nickgomes82@gmail.com>
2022-06-16 09:08:24 -0400

    Adding the ability for the tool to read subfolders one at a time

--- read.py
+++ read.py
@@ -2,6 +2,7 @@ import os
 import json
 import mysql.connector
 import getpass
+import gzip
 
 
 def safeGet(map, key, fallback):
@@ -10,6 +11,111 @@ def safeGet(map, key, fallback):
 
 	return fallback
 
+
+
+
+def readSecDataFolder(folderName):
+	#get 10-k filing's asch id's
+	subfile = open( folderName + "/sub.txt")
+
+	myline = subfile.readline()
+	values = myline.split("\t")
+
+
+	idsForAnnualReports = {}
+	while myline:
+		
+		values = myline.split("\t")
+		
+		if("10-K" in values[25]):
+			idsForAnnualReports[values[0]] = values[1]
+
+		myline = subfile.readline()
+
+	subfile.close()
+
+
+
+	#Read importan columne
+	importantColumnMap = {}
+
+	importantColumnList = [
+	"Assets",
+	"Liabilities",
+	"Goodwill",
+	"FiniteLivedIntangibleAssetsNet",
+	"EntityCommonStockSharesOutstanding"
+	]
+
+	for item in importantColumnList:
+		importantColumnMap[item] = ""
+
+
+
+	numfile = open(folderName + "/num.txt")
+
+	myline = numfile.readline()
+
+	analysisdb = {}
+
+	while myline:
+		values = myline.split("\t")
+		if(values[0] in idsForAnnualReports):
+			#print(myline)
+			if(values[1] in importantColumnMap):
+				cik = idsForAnnualReports[values[0]]
+				idToPrint =  cik
+				if(cik in cikToTickerMap):
+					ticker = cikToTickerMap[cik]
+					
+					if(not ticker in analysisdb):
+						analysisdb[ticker] = {}
+
+					period = values[4][0:4] #trim for just the year
+					valueName = values[1]
+
+					if(not period in analysisdb[ticker]):
+						analysisdb[ticker][period] = {}
+				
+					analysisdb[ticker][period][valueName] = values[7]
+					#print(myline)
+
+
+
+
+		myline = numfile.readline()
+
+	numfile.close()
+
+
+	for ticker in analysisdb:
+		tickerYears = analysisdb[ticker]
+		for year in tickerYears:
+			dataMap = tickerYears[year]
+
+			assets = safeGet(dataMap, "Assets", 0.0)
+			liabilities = safeGet(dataMap, "Liabilities", 0.0)
+			goodwill = safeGet(dataMap, "Goodwill", 0.0)
+			intangibles = safeGet(dataMap, "FiniteLivedIntangibleAssetsNet", 0.0)
+			shares = safeGet(dataMap, "EntityCommonStockSharesOutstanding", 0.0)
+
+			sqlQuery = f"INSERT INTO annual_reports (ticker, year, assets, liabilities, goodwill, intangibles, shares)"
+			sqlQuery += f" VALUES ('{ticker}','{year}', '{assets}', '{liabilities}', '{goodwill}', '{intangibles}', '{shares}')"
+
+			try:
+				cursor.execute(sqlQuery)
+			except mysql.connector.Error as err:
+				print(err.msg)
+				print(sqlQuery)
+				#print(sqlQuery)
+
+	cnx.commit()
+	print("Done reading folder: " + folderName)
+
+
+
+
+##---MAIN ENTRY----
 
 #map cik to tickers
 cikToTickerMap = {}
@@ -26,45 +132,7 @@ while myline:
 cikMapFile.close()
 
 
-#get 10-k filing's asch id's
-subfile = open("sub.txt")
-
-myline = subfile.readline()
-values = myline.split("\t")
-
-
-idsForAnnualReports = {}
-while myline:
-	
-	values = myline.split("\t")
-	
-	if("10-K" in values[25]):
-		#print(myline)
-		idsForAnnualReports[values[0]] = values[1]
-
-	myline = subfile.readline()
-
-subfile.close()
-
-
-
-#Read importan columne
-importantColumnMap = {}
-
-importantColumnList = [
-"Assets",
-"Liabilities",
-"Goodwill",
-"FiniteLivedIntangibleAssetsNet",
-"EntityCommonStockSharesOutstanding"
-]
-
-for item in importantColumnList:
-	importantColumnMap[item] = ""
-
-
-
-
+#setup table in the db
 username = input("mysql uname: ")
 pwd = getpass.getpass("mysql pwd: ")
 
@@ -106,62 +174,10 @@ for table_name in TABLES:
 cnx.commit()
 
 
-numfile = open("num.txt")
+for subdir, dirs, files in os.walk("SourceData"):
+	for dir in dirs:
+		dirPath = "SourceData/" + dir
+		print("Read Folder: " + dirPath)
+		readSecDataFolder(dirPath)
 
-myline = numfile.readline()
-
-analysisdb = {}
-
-while myline:
-	values = myline.split("\t")
-	if(values[0] in idsForAnnualReports):
-		#print(myline)
-		if(values[1] in importantColumnMap):
-			cik = idsForAnnualReports[values[0]]
-			idToPrint =  cik
-			if(cik in cikToTickerMap):
-				ticker = cikToTickerMap[cik]
-				
-				if(not ticker in analysisdb):
-					analysisdb[ticker] = {}
-
-				period = values[4][0:4] #trim for just the year
-				valueName = values[1]
-
-				if(not period in analysisdb[ticker]):
-					analysisdb[ticker][period] = {}
-			
-				analysisdb[ticker][period][valueName] = values[7]
-				#print(myline)
-
-
-
-
-	myline = numfile.readline()
-
-numfile.close()
-
-
-for ticker in analysisdb:
-	tickerYears = analysisdb[ticker]
-	for year in tickerYears:
-		dataMap = tickerYears[year]
-
-		assets = safeGet(dataMap, "Assets", 0.0)
-		liabilities = safeGet(dataMap, "Liabilities", 0.0)
-		goodwill = safeGet(dataMap, "Goodwill", 0.0)
-		intangibles = safeGet(dataMap, "FiniteLivedIntangibleAssetsNet", 0.0)
-		shares = safeGet(dataMap, "EntityCommonStockSharesOutstanding", 0.0)
-
-		sqlQuery = f"INSERT INTO annual_reports (ticker, year, assets, liabilities, goodwill, intangibles, shares)"
-		sqlQuery += f" VALUES ('{ticker}','{year}', '{assets}', '{liabilities}', '{goodwill}', '{intangibles}', '{shares}')"
-
-		try:
-			cursor.execute(sqlQuery)
-		except mysql.connector.Error as err:
-			print(err.msg)
-			print(sqlQuery)
-			#print(sqlQuery)
-
-cnx.commit()
 print("DONE!")
